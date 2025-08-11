8460553d66f4d3450807d79abfb6d4224c52f6bc
Tanmay Joshi <138454520+joshity@users.noreply.github.com>
2025-08-11 12:37:59 +0200

    Crash fix for UIC

--- components/esp_websocket_client/esp_websocket_client.c
+++ components/esp_websocket_client/esp_websocket_client.c
@@ -1146,9 +1146,10 @@ static void esp_websocket_client_task(void *pv)
     esp_transport_close(client->transport);
     xEventGroupSetBits(client->status_bits, STOPPED_BIT);
     client->state = WEBSOCKET_STATE_UNKNOW;
-    if (client->selected_for_destroying == true) {
-        destroy_and_free_resources(client);
-    }
+    // causing Crash, As we are not using this and calling our own destroy this can be removed
+    // if (client->selected_for_destroying == true) {
+    //     destroy_and_free_resources(client);
+    // }
     vTaskDelete(NULL);
 }
 
